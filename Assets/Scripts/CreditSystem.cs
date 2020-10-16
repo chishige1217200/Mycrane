6ccaff2cb46d14afe5210b4434a82d570294d81c
chishige1217200 <daichicchi01@gmail.com>
2020-10-17 00:22:43 +0900

    [fix] Credit System, Service Button

--- Assets/Scripts/CreditSystem.cs
+++ Assets/Scripts/CreditSystem.cs
@@ -4,12 +4,12 @@ using UnityEngine.UI;
 
 public class CreditSystem : MonoBehaviour
 {
-    public static int credit = 0; //残クレジット
-    private static int nowpaid = 0; //投入金額（開始時リセット）
-    public static int nowpaidSum = 0; //投入金額合計
+    public int creditNew = 0; //新規クレジット
+    public int creditAll = 0; //全クレジット
+    public int creditDisplayed = 0; //筐体に表示されるクレジット
+    private int nowpaid = 0; //投入金額（開始時リセット）
+    public int nowpaidSum = 0; //投入金額合計
     private int[,] rateSet = new int[2, 2];  //100円1PLAY，500円6PLAYなどのプリセット?
-
-    private Type2Manager craneGameManager; //クレーンゲーム親スクリプト
 
     //For test-----------------------------------------
 
@@ -19,12 +19,10 @@ public class CreditSystem : MonoBehaviour
     //-------------------------------------------------
     void Start()
     {
-        craneGameManager = GameObject.Find("CraneGame").GetComponent<Type2Manager>();
-
-        rateSet[0, 0] = 200;
+        rateSet[0, 0] = 100;
         rateSet[0, 1] = 1;
         rateSet[1, 0] = 500;
-        rateSet[1, 1] = 3;
+        rateSet[1, 1] = 6;
 
         if ((float)rateSet[0, 0] / rateSet[0, 1] < (float)rateSet[1, 0] / rateSet[1, 1])
         {
@@ -34,8 +32,8 @@ public class CreditSystem : MonoBehaviour
 
     void Update()
     {
-            nowPaid.text = nowpaid.ToString();
-            Credit.text = credit.ToString();
+        nowPaid.text = nowpaid.ToString();
+        Credit.text = creditDisplayed.ToString();
     }
 
     public void GetPayment(int cost)
@@ -44,18 +42,28 @@ public class CreditSystem : MonoBehaviour
         nowpaidSum += cost;
         if (nowpaid % rateSet[1, 0] == 0)
         {
-            credit = credit + (nowpaid / rateSet[1, 0] * rateSet[1, 1] - nowpaid / rateSet[0, 0] * rateSet[0, 1]);
+            creditNew = nowpaid / rateSet[1, 0] * rateSet[1, 1];
         }
         else if (nowpaid % rateSet[0, 0] == 0)
         {
-            credit = credit + rateSet[0, 1];
+            creditNew = nowpaid / rateSet[0, 0] * rateSet[0, 1];
         }
-
-        if(credit > 0) Type2Manager.craneStatus = 1;
+        creditDisplayed = creditAll + creditNew;
+        SEPlayer.PlaySE(0, 1);
     }
 
-    public static void ResetNowPayment()
+    public void ResetNowPayment()
     {
-        nowpaid = 0;
+        if (nowpaid % rateSet[1, 0] == 0 || nowpaid % rateSet[0, 0] == 0) nowpaid = 0;
+        else nowpaid = nowpaid % rateSet[0, 0];
+        creditAll = creditAll + creditNew;
+        creditNew = 0;
+        creditDisplayed = creditAll + creditNew;
+    }
+
+    public void ServiceButton()
+    {
+        creditAll++;
+        creditDisplayed = creditAll + creditNew;
     }
 }
