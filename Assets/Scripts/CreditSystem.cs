15109b62bcbb7a1ac0f7233fc3b6d456fb98f3eb
chishige1217200 <daichicchi01@gmail.com>
2020-10-17 00:58:48 +0900

    [fix] 1100円11PLAY問題(500円6PLAYプレセットあり)

--- Assets/Scripts/CreditSystem.cs
+++ Assets/Scripts/CreditSystem.cs
@@ -19,10 +19,10 @@ public class CreditSystem : MonoBehaviour
     //-------------------------------------------------
     void Start()
     {
-        rateSet[0, 0] = 100;
-        rateSet[0, 1] = 1;
+        rateSet[0, 0] = 200;
+        rateSet[0, 1] = 3;
         rateSet[1, 0] = 500;
-        rateSet[1, 1] = 6;
+        rateSet[1, 1] = 1;
 
         if ((float)rateSet[0, 0] / rateSet[0, 1] < (float)rateSet[1, 0] / rateSet[1, 1])
         {
@@ -40,13 +40,17 @@ public class CreditSystem : MonoBehaviour
     {
         nowpaid += cost;
         nowpaidSum += cost;
-        if (nowpaid % rateSet[1, 0] == 0)
+        if (nowpaid % rateSet[1, 0] == 0 && creditNew < nowpaid / rateSet[1, 0] * rateSet[1, 1])
         {
             creditNew = nowpaid / rateSet[1, 0] * rateSet[1, 1];
         }
-        else if (nowpaid % rateSet[0, 0] == 0)
+        else if (nowpaid % rateSet[0, 0] == 0 && creditNew < nowpaid / rateSet[0, 0] * rateSet[0, 1])
         {
             creditNew = nowpaid / rateSet[0, 0] * rateSet[0, 1];
+        }
+        else
+        {
+            Debug.Log("あなたは損または保留をしています");
         }
         creditDisplayed = creditAll + creditNew;
         SEPlayer.PlaySE(0, 1);
