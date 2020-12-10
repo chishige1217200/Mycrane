947aba3e1eab0e1512e8b5b78e1ec11b2f7dba9d
chishige1217200 <daichicchi01@gmail.com>
2020-12-10 12:14:33 +0900

    [add] RopePoint.cs

---
+++ Assets/Scripts/RopePoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopePoint : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "UpLimit")
+        {
+
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag == "UpLimit")
+        {
+
+        }
+    }
+}
