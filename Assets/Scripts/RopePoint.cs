fae5a74b98ac00209ecbfa983bc7b58a1e68637c
chishige1217200 <daichicchi01@gmail.com>
2020-12-11 12:22:28 +0900

    [add] RopeUp

--- Assets/Scripts/RopePoint.cs
+++ Assets/Scripts/RopePoint.cs
@@ -1,28 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class RopePoint : MonoBehaviour
 {
-    Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
+    public bool parent = false; //一番上の質点かどうか
+    public bool last = false; //下から二番目の質点かどうか
+    Rigidbody rb; //Rigidbody情報
+
+    bool moveFlag = false; //上昇または下降をしているか
+
+    async void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        await Task.Delay(1000);
+        if (parent)
+            moveFlag = true;
     }
+
+    void Update()
+    {
+        RopeUp();
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "UpLimit")
         {
-
+            moveFlag = false;
+        }
+        else if (collider.tag == "UpPoint")
+        {
+            if (!parent)
+            {
+                rb.useGravity = false;
+                rb.isKinematic = true;
+                moveFlag = true;
+            }
         }
     }
+
+    /*void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "UpLimit")
+        {
+            if (parent)
+                moveFlag = false;
+        }
+    }*/
 
     void OnTriggerExit(Collider collider)
     {
         if (collider.tag == "UpLimit")
         {
-
+            if (parent)
+            {
+                if (moveFlag)
+                    moveFlag = false;
+            }
+            else
+            {
+                if (moveFlag)
+                {
+                    rb.useGravity = true;
+                    rb.isKinematic = false;
+                    moveFlag = false;
+                }
+            }
         }
     }
+
+    void RopeUp()
+    {
+        if (moveFlag)
+            this.transform.position += new Vector3(0, 0.1f, 0);
+    }
+
+    void RopeDown()
+    {
+        if (moveFlag)
+            this.transform.position -= new Vector3(0, 0.1f, 0);
+    }
+
+    void ResetRopePointPosition()
+    {
+        this.transform.position = new Vector3(0, 0, 0);
+    }
+
+
 }
