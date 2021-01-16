8d0516c9c43be1b6f13a9ff489d0f1b2d4463ac7
chishige1217200 <daichicchi01@gmail.com>
2021-01-16 16:22:10 +0900

    [add] 終了ボタンの設置

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //　ゲーム終了ボタンを押したら実行する
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		Application.OpenURL("http://www.yahoo.co.jp/");
+#else
+		Application.Quit();
+#endif
+    }
+
+}
