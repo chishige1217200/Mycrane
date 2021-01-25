b996f7339aa781224c0b9cf9663ec8838e1b87fe
chishige1217200 <daichicchi01@gmail.com>
2021-01-25 20:38:24 +0900

    [update] 多重に獲得音がなるバグ

--- Assets/Scripts/GetPoint.cs
+++ Assets/Scripts/GetPoint.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class GetPoint : MonoBehaviour
 {
     private SEPlayer _SEPlayer;
     private CreditSystem creditSystem;
     public int getSoundNum = -1;
+    private bool checkFlag = true; // trueのときのみ獲得口チェック
+
+    async void Update()
+    {
+        if (!checkFlag)
+        {
+            await Task.Delay(5000);
+            checkFlag = true;
+        }
+    }
 
     public void GetSEPlayer(SEPlayer s)
     {
@@ -20,18 +31,22 @@ public class GetPoint : MonoBehaviour
 
     void OnTriggerEnter()
     {
-        if (getSoundNum != -1)
-            _SEPlayer.PlaySE(getSoundNum, 1);
-        switch (creditSystem.probabilityMode)
+        if (checkFlag)
         {
-            case 2:
-            case 3:
-                creditSystem.ResetCreditProbability();
-                break;
-            case 4:
-            case 5:
-                creditSystem.ResetCostProbability();
-                break;
+            checkFlag = false;
+            if (getSoundNum != -1)
+                _SEPlayer.PlaySE(getSoundNum, 1);
+            switch (creditSystem.probabilityMode)
+            {
+                case 2:
+                case 3:
+                    creditSystem.ResetCreditProbability();
+                    break;
+                case 4:
+                case 5:
+                    creditSystem.ResetCostProbability();
+                    break;
+            }
         }
     }
 
