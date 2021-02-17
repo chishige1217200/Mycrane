59e0007acf9fb1cf28f333d20d15359905739ccd
chishige1217200 <daichicchi01@gmail.com>
2021-02-17 15:28:18 +0900

    [update] cs名前変更

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MousePointerpos : MonoBehaviour
+public class Lever : MonoBehaviour
 {
     //Canvasの変数
     public Canvas canvas;
