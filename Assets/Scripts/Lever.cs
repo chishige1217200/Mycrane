6fa623ef0604b0c2b1daab4a0040377f0b6c5dfb
chishige1217200 <daichicchi01@gmail.com>
2021-02-19 17:34:18 +0900

    [fix] レバーと逆方向に移動してしまう問題

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -38,54 +38,61 @@ public class Lever : MonoBehaviour
              * 出力先はMousePos
              */
             RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, Input.mousePosition, canvas.worldCamera, out MousePos);
+            radian = (float)Math.Atan2(MousePos.y - init.y, MousePos.x - init.x); //x-z平面のtanの値計算
             // RectTransformの座標を更新
             if (Vector2.Distance(init, MousePos) <= leverRange)
                 pointer.anchoredPosition = new Vector2(MousePos.x, MousePos.y);
             else if (Vector2.Distance(init, MousePos) > leverRange) // 可動域よりも外側にカーソルがある場合
             {
-                radian = (float)Math.Atan2(MousePos.y - init.y, MousePos.x - init.x); //x-z平面のtanの値計算
                 pointer.anchoredPosition = new Vector2(init.x + (leverRange * (float)Math.Cos(radian)), init.y + (leverRange * (float)Math.Sin(radian))); // 基準値にオフセットを足して座標とする
             }
 
             //Flagの処理
-            if (Vector2.Distance(init, MousePos) >= leverRange / 2)
+            if (Vector2.Distance(init, MousePos) >= leverRange / 2.0f)
             {
+                Debug.Log(radian / Math.PI + "pi");
                 for (int i = 0; i < 7; i++)
                 {
                     if (radian > (-7 + 2 * i) * Math.PI / 8 && radian <= (-5 + 2 * i) * Math.PI / 8) // レバーの方向を検知，Flagを場合分け．radianは弧度法による値:-Pi to Pi
                     {
-                        InitializeFlag();
                         switch (i)
                         {
                             case 0:
+                                InitializeFlag();
                                 leftFlag = true;
                                 forwardFlag = true;
                                 break;
                             case 1:
+                                InitializeFlag();
                                 forwardFlag = true;
                                 break;
                             case 2:
+                                InitializeFlag();
                                 rightFlag = true;
                                 forwardFlag = true;
                                 break;
                             case 3:
+                                InitializeFlag();
                                 rightFlag = true;
                                 break;
                             case 4:
+                                InitializeFlag();
                                 rightFlag = true;
                                 backFlag = true;
                                 break;
                             case 5:
+                                InitializeFlag();
                                 backFlag = true;
                                 break;
                             case 6:
+                                InitializeFlag();
                                 leftFlag = true;
                                 backFlag = true;
                                 break;
                         }
                     }
                 }
-                if (radian > 7 * Math.PI / 8 && radian <= Math.PI || radian > -Math.PI && radian <= -7 * Math.PI) // 左端は-と+が交じるため特別な処理
+                if (radian > 7 * Math.PI / 8 && radian <= Math.PI || radian >= -Math.PI && radian <= -7 * Math.PI / 8) // 左端は-と+が交じるため特別な処理
                 {
                     InitializeFlag();
                     leftFlag = true;
