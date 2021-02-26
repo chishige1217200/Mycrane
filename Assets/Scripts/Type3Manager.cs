867df07b050267fa4ea032b9b7aa14534a8eab86
chishige1217200 <daichicchi01@gmail.com>
2021-02-26 16:35:13 +0900

    [update] Type3 ランダム確率テスト

--- Assets/Scripts/Type3Manager.cs
+++ Assets/Scripts/Type3Manager.cs
@@ -13,8 +13,8 @@ public class Type3Manager : MonoBehaviour
     float catchArmpowersuccess = 100; //同確率時
     float upArmpowersuccess = 100; //同確率時
     float backArmpowersuccess = 100; //同確率時
-    int soundType = 1; //0:CARINO 1:CARINO4 2:BAMBINO 3:neomini
-    float audioPitch = 1f; //サウンドのピッチ
+    int soundType = 0; //0:CARINO 1:CARINO4 2:BAMBINO 3:neomini
+    float audioPitch = 1.1f; //サウンドのピッチ
     private bool[] instanceFlag = new bool[13];
     public bool buttonFlag = false; // trueならボタンをクリックしているかキーボードを押下している
     public bool probability; // 確率判定用
