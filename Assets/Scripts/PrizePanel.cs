259423dbc08a2d8cecb5ef7fca5ac2edabc0df10
chishige1217200 <daichicchi01@gmail.com>
2021-03-20 19:26:46 +0900

    [add] 獲得表示

---
+++ Assets/Scripts/PrizePanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrizePanel : MonoBehaviour
+{
+    GameObject canvas;
+    Text prizeText;
+    void Start()
+    {
+        canvas = this.transform.Find("Canvas").gameObject;
+        prizeText = this.transform.Find("Canvas").Find("Panel").Find("PrizeText").GetComponent<Text>();
+    }
+
+    public void SetPrizeName(string name)
+    {
+        prizeText.text = "「" + name + "」を獲得しました！";
+    }
+
+
+    public void PanelActive(bool t)
+    {
+        if (!canvas.activeSelf && t) canvas.SetActive(true);
+        if (canvas.activeSelf && !t) canvas.SetActive(false);
+    }
+}
