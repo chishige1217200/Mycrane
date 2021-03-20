257b903b772d88d9aee86dcf311268e69937ff39
chishige1217200 <daichicchi01@gmail.com>
2021-03-21 01:33:05 +0900

    [add] 獲得日時表示

--- Assets/Scripts/PrizePanel.cs
+++ Assets/Scripts/PrizePanel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,17 +8,19 @@ public class PrizePanel : MonoBehaviour
 {
     GameObject canvas;
     Text prizeText;
+    Text dateText;
     void Start()
     {
         canvas = this.transform.Find("Canvas").gameObject;
         prizeText = this.transform.Find("Canvas").Find("Panel").Find("PrizeText").GetComponent<Text>();
+        dateText = this.transform.Find("Canvas").Find("Panel").Find("DateText").GetComponent<Text>();
     }
 
     public void SetPrizeName(string name)
     {
         prizeText.text = "「" + name + "」\nを獲得しました！";
+        dateText.text = "獲得日時:" + DateTime.Now.ToLongDateString() + DateTime.Now.ToLongTimeString();
     }
-
 
     public void PanelActive(bool t)
     {
