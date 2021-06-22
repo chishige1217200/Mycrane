78b80bc7ba58ce6ebe2ef307ca687bc336b8b6fa
chishige1217200 <daichicchi01@gmail.com>
2021-06-22 23:45:24 +0900

    [update] Type4 Videoパネル削減

--- Assets/Scripts/VideoPlay.cs
+++ Assets/Scripts/VideoPlay.cs
@@ -23,23 +23,24 @@ public class VideoPlay : MonoBehaviour
             }
         }
 
-        if (videoPlayer[0].isPrepared && videoPlayer[1].isPrepared && videoPlayFlag)
-        {
-            videoPlayFlag = false;
-            videoPlayer[0].Play();
-            videoPlayer[1].Play();
-        }
+        if (videoPlayFlag)
+            if (videoPlayer[0].isPrepared/* && videoPlayer[1].isPrepared */)
+            {
+                videoPlayFlag = false;
+                videoPlayer[0].Play();
+                //videoPlayer[1].Play();
+            }
     }
 
     public void PlayVideo(int num) // videoPlayer.isPreparedがfalseのときに呼び出してはいけない可能性
     {
         videoPlayFlag = false;
         videoPlayer[0].Stop();
-        videoPlayer[1].Stop();
+        //videoPlayer[1].Stop();
         videoPlayer[0].clip = videoClips[num];
-        videoPlayer[1].clip = videoClips[num];
+        //videoPlayer[1].clip = videoClips[num];
         videoPlayer[0].Prepare();
-        videoPlayer[1].Prepare();
+        //videoPlayer[1].Prepare();
         videoPlayFlag = true;
     }
 }
