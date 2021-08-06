52bf1ac6f6d2c26c97e0d0ab5f1de56fe71fabfb
chishige1217200 <daichicchi01@gmail.com>
2021-08-07 01:53:27 +0900

    [add] Type4 待機時Video同期機能

--- Assets/Scripts/Type4Selecter.cs
+++ Assets/Scripts/Type4Selecter.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
 
 public class Type4Selecter : MonoBehaviour
 {
     [SerializeField] Type4Manager[] manager = new Type4Manager[2];
+    [SerializeField] Type4VideoPlay[] videoPlay = new Type4VideoPlay[2];
     BGMPlayer _BGMPlayer;
     SEPlayer[] _SEPlayer = new SEPlayer[2];
+    float playTime = 0; //ビデオ再生時間
 
     void Start()
     {
         _BGMPlayer = this.transform.Find("BGM").GetComponent<BGMPlayer>();
         _SEPlayer[0] = this.transform.Find("1P").Find("SE").GetComponent<SEPlayer>();
         _SEPlayer[1] = this.transform.Find("2P").Find("SE").GetComponent<SEPlayer>();
+        RandomVideoPlay();
     }
 
     void Update()
@@ -35,6 +39,21 @@ public class Type4Selecter : MonoBehaviour
                 _BGMPlayer.PlayBGM(1);
             }
         }
+    }
+
+    async void RandomVideoPlay()
+    {
+        int randomValue;
+
+        while (true)
+        {
+            randomValue = Random.Range(0, videoPlay[0].videoClips.Length);
+            playTime = Random.Range(3, 8);
+            if (manager[0].craneStatus == 0 && videoPlay[0].randomMode) videoPlay[0].PlayVideo(randomValue);
+            if (manager[1].craneStatus == 0 && videoPlay[1].randomMode) videoPlay[1].PlayVideo(randomValue);
+            await Task.Delay((int)(playTime * 1000));
+        }
+
     }
 
     public Type4Manager GetManager(int num)
--- Assets/Scripts/Type4VideoPlay.cs
+++ Assets/Scripts/Type4VideoPlay.cs
@@ -4,43 +4,14 @@ using System.Threading.Tasks;
 
 public class Type4VideoPlay : MonoBehaviour
 {
-    public VideoPlayer[] videoPlayer; //オーディオ情報の格納
+    public VideoPlayer videoPlayer; //ビデオ情報の格納
     public VideoClip[] videoClips;
     public bool randomMode = false; //trueのときランダムにビデオを再生
-    bool videoPlayFlag = false;
-    float playTime = 0; //ビデオ再生時間
-    float startTime;
-
-    void Update()
-    {
-        if (randomMode)
-        {
-            if (playTime - Time.time + startTime < 0)
-            {
-                playTime = Random.Range(3, 8);
-                startTime = Time.time;
-                PlayVideo(Random.Range(0, videoClips.Length));
-            }
-        }
-
-        if (videoPlayFlag)
-            if (videoPlayer[0].isPrepared/* && videoPlayer[1].isPrepared */)
-            {
-                videoPlayFlag = false;
-                videoPlayer[0].Play();
-                //videoPlayer[1].Play();
-            }
-    }
 
     public void PlayVideo(int num) // videoPlayer.isPreparedがfalseのときに呼び出してはいけない可能性
     {
-        videoPlayFlag = false;
-        videoPlayer[0].Stop();
-        //videoPlayer[1].Stop();
-        videoPlayer[0].clip = videoClips[num];
-        //videoPlayer[1].clip = videoClips[num];
-        videoPlayer[0].Prepare();
-        //videoPlayer[1].Prepare();
-        videoPlayFlag = true;
+        videoPlayer.Stop();
+        videoPlayer.clip = videoClips[num];
+        videoPlayer.Play();
     }
 }
