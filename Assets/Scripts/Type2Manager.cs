4595775688503509f2c9ad0760a42a9dcbd3d833
chishige1217200 <daichicchi01@gmail.com>
2021-10-30 13:25:49 +0900

    Update: Type2 冗長な記述を削除

--- Assets/Scripts/Type2Manager.cs
+++ Assets/Scripts/Type2Manager.cs
@@ -234,28 +234,25 @@ public class Type2Manager : CraneManager
 
             if (craneStatus == 7)
             {
-                if (craneStatus == 7) //awaitによる時差実行を防止
+                switch (soundType)
                 {
-                    switch (soundType)
-                    {
-                        case 0:
-                            sp.Stop(1); //アーム下降音再生停止;
-                            sp.Play(2, 1); //アーム掴む音再生;
-                            break;
-                        case 1:
-                            sp.Stop(8);
-                            break;
-                        case 2:
-                            sp.Stop(12);
-                            break;
-                    }
-                    if (probability) armPower = armPowerConfigSuccess[0];
-                    else armPower = armPowerConfig[0];
-                    armController.MotorPower(armPower);
-                    armController.Close();
-                    await Task.Delay(1000);
-                    if (craneStatus == 7) craneStatus = 8; //awaitによる時差実行を防止
+                    case 0:
+                        sp.Stop(1); //アーム下降音再生停止;
+                        sp.Play(2, 1); //アーム掴む音再生;
+                        break;
+                    case 1:
+                        sp.Stop(8);
+                        break;
+                    case 2:
+                        sp.Stop(12);
+                        break;
                 }
+                if (probability) armPower = armPowerConfigSuccess[0];
+                else armPower = armPowerConfig[0];
+                armController.MotorPower(armPower);
+                armController.Close();
+                await Task.Delay(1000);
+                if (craneStatus == 7) craneStatus = 8; //awaitによる時差実行を防止
                 //アーム掴む;
             }
 
