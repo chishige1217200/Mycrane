7142f8483aa28bfe43025c377817952132b912cb
chishige1217200 <daichicchi01@gmail.com>
2022-01-08 17:40:34 +0900

    Update: アーム上昇時の離す振り分けを軽減

--- Assets/Scripts/Type10Manager.cs
+++ Assets/Scripts/Type10Manager.cs
@@ -315,7 +315,7 @@ public class Type10Manager : CraneManager
                     }
                     ropeManager.Up();
                     await Task.Delay(1500);
-                    if (!probability && UnityEngine.Random.Range(0, 2) == 0 && craneStatus == 8 && support.prizeCount > 0) armController.Release(); //上昇中に離す振り分け(autoPower設定時のみ)
+                    if (!probability && UnityEngine.Random.Range(0, 3) == 0 && craneStatus == 8 && support.prizeCount > 0) armController.Release(); //上昇中に離す振り分け(autoPower設定時のみ)
                 }
                 if (probability && armPower > armPowerConfigSuccess[1])
                 {
