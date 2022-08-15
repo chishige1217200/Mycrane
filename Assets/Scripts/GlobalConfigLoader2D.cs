9c354553c14d0ac55e56b400e476146f7bc5b77d
chishige1217200 <daichicchi01@gmail.com>
2022-08-15 16:37:34 +0900

    Fix: 2DモードでUIが正常に表示されない場合があったのを修正

---
+++ Assets/Scripts/GlobalConfigLoader2D.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalConfigLoader2D : MonoBehaviour
+{
+    void Start()
+    {
+        CraneManager.useUI = true;
+    }
+}
