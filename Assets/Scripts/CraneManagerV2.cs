f6b206c1581950ebe1853d8176df2b9999cc43ce
chishige1217200 <daichicchi01@gmail.com>
2022-11-04 21:57:32 +0900

    Update: テトラ基本動作

---
+++ Assets/Scripts/CraneManagerV2.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CraneManagerV2 : MonoBehaviour
+{
+    protected int craneType;
+    public int craneStatus //クレーン状態
+    {
+        get { return _craneStatus; }
+
+        set
+        {
+            LastStatusEvent(_craneStatus);
+            _craneStatus = value;
+            FirstStatusEvent(_craneStatus);
+        }
+    } //クレーン状態
+    private int _craneStatus = 0;
+    protected bool probability; //確率判定用
+    protected int getSoundNum = -1;
+    public CreditSystem creditSystem;
+    public SEPlayer sp;
+    public CraneBox craneBox;
+    protected GetPointV2 getPoint;
+    public MachineHost host;
+    protected GameObject canvas;
+    public bool isHibernate = false;
+    public static bool useUI = true;
+
+    public virtual void GetPrize()
+    {
+        Debug.Log("CraneManager GetPrize");
+        switch (creditSystem.probabilityMode)
+        {
+            case 2:
+            case 3:
+                creditSystem.ResetCreditProbability();
+                break;
+            case 4:
+            case 5:
+                creditSystem.ResetCostProbability();
+                break;
+        }
+        if (getSoundNum != -1)
+        {
+            if (!sp.audioSource[getSoundNum].isPlaying)
+                sp.Play(getSoundNum, 1);
+        }
+    }
+
+    protected abstract void DetectKey(int num);
+    public abstract void ButtonDown(int num);
+    public abstract void InsertCoin();
+    protected abstract void FirstStatusEvent(int status);
+
+
+    protected abstract void LastStatusEvent(int status);
+    
+
+    public int GetCType()
+    {
+        return craneType;
+    }
+
+    public bool GetProbability()
+    {
+        return probability;
+    }
+}
---
+++ Assets/Scripts/GetPointV2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class GetPointV2 : MonoBehaviour
+{
+    CraneManagerV2 craneManager;
+    [SerializeField] bool autoDestroy = false; // 自動的に景品を消去する
+    PrizePanel panel;
+    Prize prize;
+    MissionMode mission;
+    private IEnumerator DelayCoroutine(float miliseconds, Action action)
+    {
+        yield return new WaitForSeconds(miliseconds / 1000f);
+        action?.Invoke();
+    }
+    void Start()
+    {
+        GameObject gb = GameObject.Find("GameManager");
+        if (gb.TryGetComponent(out panel)) panel = gb.GetComponent<PrizePanel>();
+        if (gb.TryGetComponent(out mission)) mission = gb.GetComponent<MissionMode>();
+    }
+
+    public void SetManager(CraneManagerV2 c)
+    {
+        craneManager = c;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "prize")
+        {
+            craneManager.GetPrize();
+            if (collider.gameObject.TryGetComponent(out prize) && autoDestroy)
+            {
+                if (mission == null)
+                {
+                    panel.SetPrizeName(prize.prizeName);
+                    panel.PanelActive(true);
+                }
+                else
+                {
+                    mission.GameClear();
+                }
+                StartCoroutine(DelayCoroutine(5000, () =>
+                {
+                    if (prize.destroyObject != null) Destroy(prize.destroyObject);
+                    else Destroy(collider.gameObject);
+                }));
+            }
+        }
+    }
+}
