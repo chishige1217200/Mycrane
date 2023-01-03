a26bc1beec72d93473f820d517b3df286df185dc
chishige1217200 <daichicchi01@gmail.com>
2023-01-03 23:36:30 +0900

    Fix: Tubeの追従座標が不適切

--- Assets/Scripts/Tube.cs
+++ Assets/Scripts/Tube.cs
@@ -8,18 +8,20 @@ public class Tube : MonoBehaviour
     GameObject craneUnit;
     public bool parent = false; //一番上の質点かどうか
     public bool last = false; //下から二番目の質点かどうか
+    float craneUnitdiff = 0f; // craneUnitオブジェクトとのy座標差
 
     void Start()
     {
         craneUnit = transform.parent.Find("TubePoint").gameObject;
+        craneUnitdiff = Mathf.Abs(craneUnit.transform.position.y - transform.position.y);
     }
 
     void FixedUpdate()
     {
         if (!parent)
         {
-            if (craneUnit.transform.position.y > tube.transform.position.y - tube.transform.lossyScale.y * 2.0f)
-                transform.position = new Vector3(transform.position.x, craneUnit.transform.position.y, transform.position.z);
+            if (craneUnit.transform.position.y > tube.transform.position.y - (tube.transform.lossyScale.y + transform.lossyScale.y * 2.0f))
+                transform.position = new Vector3(transform.position.x, craneUnit.transform.position.y + craneUnitdiff, transform.position.z);
         }
     }
 }
